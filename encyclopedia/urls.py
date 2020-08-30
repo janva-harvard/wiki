432587b71d74465e69210d79df59bcd55c85762d
Janne Väisänen <janva415@student.liu.se>
2020-08-30 12:05:40 +0300

    Add editing and adding new article capabilities

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:entry>", views.article, name="entry"),
-    path("editor/newwiki", views.newwiki, name="newwiki")
+    path("editor/newwiki", views.newwiki, name="newwiki"),
+    path("editor/edit", views.edit, name="editEntry")
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -14,11 +14,18 @@ class SearchForm(forms.Form):
 
 
 class newWikiForm(forms.Form):
-    titleField = forms.CharField(label="Wiki title")
-    textArea = forms.CharField(label="",
-                               widget=forms.Textarea(attrs={'rows': 1,
-                                                            'cols': 40,
-                                                            'style': 'height: 5em;'}))
+    wikiTitle = forms.CharField(label="Wiki title")
+    wikiContent = forms.CharField(label="",
+                                  widget=forms.Textarea(attrs={'rows': 1,
+                                                               'cols': 40,
+                                                               'style': 'height: 5em;'}))
+
+
+class EditForm(forms.Form):
+    wikiContent = forms.CharField(label="",
+                                  widget=forms.Textarea(attrs={'rows': 1,
+                                                               'cols': 40,
+                                                               'style': 'height: 10em;'}))
 
 
 def handleSearchRequest(request):
@@ -32,17 +39,17 @@ def handleSearchRequest(request):
             # return article(request, query)
             # return handle_search(request, query)
         else:
-            heading = "No exact match found. Maybe you can try.."
+            title = "No exact match found. Maybe you can try.."
             near_matches = list(
                 filter(lambda entry: query.lower() in entry.lower(), util.list_entries()))
             if not near_matches:
-                heading = "Can't find close match try annother query!"
+                title = "Can't find close match try annother query!"
 
             # code duplication fixme refactor
             return render(request, "encyclopedia/index.html", {
                 "entries": near_matches,
                 "search_form": SearchForm(),
-                "heading": heading})
+                "title": title})
 
 
 def index(request):
@@ -57,7 +64,7 @@ def index(request):
 
 
 def article(request, entry):
-    print("called")
+    request.session["article"] = entry
     converter = Markdown()
     the_entry = util.get_entry(entry)
     if(the_entry == None):
@@ -65,17 +72,61 @@ def article(request, entry):
             "entry": entry,
             "search_form": SearchForm(),
         })
-    html = converter.convert(the_entry)
+    entry_as_html = converter.convert(the_entry)
 
     return render(request, "encyclopedia/article.html", {
         # code duplication search_form attribute potential issue
-        "entry": html, "search_form": SearchForm()
+        "entry": entry_as_html, "search_form": SearchForm()
         # "entry": util.get_entry(entry)
     })
 
 
 def newwiki(request):
-    return render(request, "encyclopedia/newwiki.html", {
-        "search_form": SearchForm(),
-        "new_form":  newWikiForm()
+    if request.method == "POST":
+        form = newWikiForm(request.POST)
+        if form.is_valid():
+            print("Form is valid************************")
+            title = form.cleaned_data["wikiTitle"]
+            content = form.cleaned_data["wikiContent"]
+            # check for duplicates
+            if util.get_entry(title) == None:
+                # save to db
+                util.save_entry(title, content)
+            else:
+                print("alredy exists************************")
+                # Hmm might be a problem
+                return render(request, "encyclopedia/newwiki.html", {
+                    "search_form": SearchForm(),
+                    "new_form":  newWikiForm(),
+                    "duplicateError": True
+                })
+            return HttpResponseRedirect(reverse("wiki:index"), {
+                "duplicateError": False
+            })
+
+    else:
+        return render(request, "encyclopedia/newwiki.html", {
+            "search_form": SearchForm(),
+            "new_form":  newWikiForm(),
+            "duplicateError": False
+        })
+
+
+def edit(request):
+    if request.method == "POST":
+        form = EditForm(request.POST)
+        if form.is_valid():
+            article = form.cleaned_data["wikiContent"]
+            title = request.session["article"]
+            util.save_entry(title, article)
+            return HttpResponseRedirect(reverse("wiki:index"), {
+                "duplicateError": False
+            })
+        return HttpResponseRedirect(reverse("wiki:index"), {
+            "duplicateError": False
+        })
+
+    return render(request, "encyclopedia/edit_entry.html", {
+        "editForm": EditForm({"wikiContent": util.get_entry(request.session["article"])}),
+        "title": request.session["article"],
     })
